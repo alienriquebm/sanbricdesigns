596efec3702b650962777bcba488b09e91eb2a68
Ali Enrique Briceño <alienriquebm@gmail.com>
2022-11-05 12:09:06 -0400

    added primary color

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,3 +1,9 @@
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  color: ${({ theme }) => theme.colors.gray400};
+`;
+
 export default function Home() {
-  return <div>Sanbric designs new app</div>;
+  return <StyledWrapper>Sanbric designs new app</StyledWrapper>;
 }
